f79a6a49a53641ea673ff71193ca35ab1bcfdbdd
혜니 <@DESKTOP-EAPKJ93>
2023-09-06 17:20:24 +0900

    8/27 수업 + review

--- src/section11/Polymorphism06.java
+++ src/section11/Polymorphism06.java
@@ -2,14 +2,14 @@ package section11;
 
 /*
  * ****instanceof 연산자
+
  * 	객체가 특정 클래스 또는 인터페이스의 인스턴스인지 여부를 확인하는 연산자
- * 
- * 
  * 
  */
 
 
 import java.util.Scanner;
+
 
 import section11.access3.Ade;
 import section11.access3.Coffee;
@@ -35,8 +35,8 @@ public class Polymorphism06 {
 		Drink drink = chooseDrink(chooseNum);
 		
 		if(drink instanceof Ade) {
-			Ade ade = (Ade)drink;
-			ade.setZero(true);
+			Ade ade = (Ade)drink;		// 자동형변환된 부모객체를 다시 자식객체로 강제형변환시킨것
+			ade.setZero(true);			// 그로인해, 자식객체에만 있는 setZero메서드를 호출할 수 있음
 			ade.getInfo();
 			ade.getDiscountInfo();
 		} else if(drink instanceof Coffee) {
@@ -57,7 +57,7 @@ public class Polymorphism06 {
 	 * 
 	 */
 	public static Drink chooseDrink(int chooseNum) {
-		Drink drink = null;
+		Drink drink = null;		//  지역변수이며, null로 초기화한 상태
 		
 		if(chooseNum == 1) {
 			drink = new Ade();
